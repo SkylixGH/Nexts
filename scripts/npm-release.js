ca05dbd3cb3e9671a84db07b00123c3b538a4b79
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-02-26 00:01:39 -0500

    Added publish script

---
+++ scripts/npm-release.js
@@ -0,0 +1,28 @@
+import {info} from './util/logger.js'
+import getProjects from './util/getProjects.js'
+import cliGet from './util/cliGet.js'
+import {spawn} from 'child_process'
+
+info('NPM Release')
+
+const projects = await getProjects()
+const npmExtraArgs = await cliGet('Extra NPM Arguments')
+let projectIndex = 0
+
+const nextProject = () => {
+	const project = projects.paths[projectIndex]
+	const proc = spawn('npm' + (process.platform === 'win32' ? '.cmd' : ''), ['publish', '--access', 'public', npmExtraArgs.split(' ')], {cwd: project, stdio: 'inherit'})
+
+	proc.on('exit', () => {
+		projectIndex++
+
+		if (projectIndex >= projects.paths.length) {
+			info('All projects have been released')
+			process.exit(0)
+		}
+
+		nextProject()
+	})
+}
+
+nextProject()
