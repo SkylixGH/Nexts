032a4d49b6ba9f5892a0bf98a4f4908c2d9c09de
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-03-02 07:21:15 -0500

    Added basic publish environment creation

--- packages/cli-service/src/commands/publishCMD.ts
+++ packages/cli-service/src/commands/publishCMD.ts
@@ -2,6 +2,8 @@ import {Argv} from 'yargs';
 import logger from '@nexts-stack/logger';
 import fsSync from 'fs';
 import readConfig from '../misc/readConfig';
+import path from 'path';
+import crashError from '../misc/crashError';
 
 interface Flags {
 	/**
@@ -33,7 +35,34 @@ export default function publishCMD(program: Argv<Flags>) {
 			logger.log('Checking environment for publishing packages');
 
 			const config = await readConfig(argv.path, argv.config);
+			logger.log(`Moving build files`);
 
-			logger.log('Moving build files');
+			if (!config.packages || config.packages.length === 0) {
+				logger.error('No packages to publish');
+				process.exit(1);
+			}
+
+			config.packages.forEach((pkg) => {
+				const publishBuildLocation = path.join(process.cwd(), argv.path, '.nexts', 'publish', pkg.path);
+				let buildCopy = 0;
+
+				const tryNextBuild = () => {
+					if (fsSync.existsSync(path.join(process.cwd(), argv.path, '.nexts', 'publish', pkg.path))) {
+						buildCopy++;
+						tryNextBuild();
+					}
+				};
+
+				try {
+					fsSync.mkdirSync(publishBuildLocation, {
+						recursive: true,
+					});
+				} catch (e) {
+					logger.error(`Could not create publish build location`);
+					crashError(e);
+
+					process.exit(1);
+				}
+			});
 		});
 }
