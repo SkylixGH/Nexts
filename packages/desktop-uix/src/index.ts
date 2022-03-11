7c20e84dfd62aa8bf64172aa228cf35801153616
XFaonAE <xfaonae@protonmail.com>
2022-03-11 10:06:16 -0500

    Dev server frame

--- packages/desktop-uix/src/index.ts
+++ packages/desktop-uix/src/index.ts
@@ -8,15 +8,18 @@ if (!isRenderer) {
 
 window.onerror = (msg, url, lineNo, columnNo, error) => {
 	document.body.innerHTML = [
+		'<div style="width: 100vw; height: 32px; background: #202020; -webkit-app-region: drag; margin-top: -10px; margin-left: -10px; display: flex; align-items: center; justify-content: flex-end;">',
+		'	<button onmouseover="this.style.color = \'#fff\'; this.style.background = \'#FF5555\'" onmouseleave="this.style.color = \'#999\'; this.style.background = \'transparent\'" class="button" style="cursor: pointer; -webkit-app-region: no-drag; width: 45px; height: 32px; color: #999; background: transparent; border: none;" onclick="window.close()">✕</button>',
+		'</div>',
 		'<h1 style="font-family: sans-serif">Failed to start ReactJS</h1>',
 		'<p style="font-family: sans-serif">',
 		'	<pre style="font-family: monospace; white-space: pre-wrap">',
 		error?.stack,
 		'	</pre>',
 		'</p>',
-	].join('')
+	].join('\n')
 
-	document.body.style.background = '#121212'
+	document.body.style.background = '#272727'
 	document.body.style.color = '#fff'
 }
 
