bc950d416b07cc0843673b941df8617370a05efb
XFaonAE <xfaonae@protonmail.com>
2022-03-16 15:09:36 -0400

    example(*): Updated app to a simple context app

--- scratch/apps/desktop/src/Root.tsx
+++ scratch/apps/desktop/src/Root.tsx
@@ -1,6 +1,47 @@
-import {App, Ring, useMenu} from '@nexts-stack/desktop-uix';
+import {App, Progress, Ring, useMenu} from '@nexts-stack/desktop-uix';
 import React from 'react';
 import './styles.scss';
+import Plus from '@iconify/icons-fluent/add-16-regular';
+import Sub from '@iconify/icons-fluent/subtract-16-regular'
+
+/**
+ *
+ */
+function useMicro() {
+	const [volume, setVolume] = React.useState(0);
+
+	navigator.mediaDevices.getUserMedia({
+		audio: true,
+		video: false,
+	})
+		.then(function(stream) {
+			const audioContext = new AudioContext();
+			const analyser = audioContext.createAnalyser();
+			const microphone = audioContext.createMediaStreamSource(stream);
+			const scriptProcessor = audioContext.createScriptProcessor(2048, 1, 1);
+
+			analyser.smoothingTimeConstant = 0.8;
+			analyser.fftSize = 1024;
+
+			microphone.connect(analyser);
+			analyser.connect(scriptProcessor);
+			scriptProcessor.connect(audioContext.destination);
+			scriptProcessor.onaudioprocess = function() {
+				const array = new Uint8Array(analyser.frequencyBinCount);
+				analyser.getByteFrequencyData(array);
+				const arraySum = array.reduce((a, value) => a + value, 0);
+				const average = arraySum / array.length;
+				setVolume(Math.round(average));
+				// colorPids(average);
+			};
+		})
+		.catch(function(err) {
+			/* handle the error */
+			console.error(err);
+		});
+
+	return volume;
+}
 
 /**
  * This is the app root component.
@@ -10,15 +51,67 @@ export default function Root() {
 	const [lines, setLines] = React.useState('1');
 	const [linesScrollTop, setLinesScrollTop] = React.useState(0);
 	const lcRef = React.useRef<HTMLTextAreaElement>(null);
+	const [size, setSize] = React.useState(1);
 	const menu = useMenu();
-	const [size, setSize] = React.useState(10);
 
 	return (
 		<App center flowDirection={'row'}>
-			<Ring size={size} />
+			<div style={{
+				width: '50vw',
+				position: 'fixed',
+				top: '10px',
+				left: '10px',
+				display: 'flex',
+				gap: '10px',
+				alignItems: 'center',
+			}}>
+				<Progress value={Math.floor((size / 6) * 100)} mode="determinate" />
+				<Ring size={9} />
+			</div>
+
+			<div style={{
+				width: '100px',
+				height: '100px',
+				outline: '4px solid #fff',
+				borderRadius: '100%',
+				transform: `scale(${size})`,
+				transition: '100ms'
+			}} onContextMenu={(event) => {
+				event.preventDefault();
+
+				menu.open({
+					header: [
+						{
+							icon: {
+								src: Plus,
+							},
+							action: () => {
+								if (size > 5) return;
+								setSize((s) => s + 1);
+							},
+						},
+						{
+							icon: {
+								src: Sub,
+							},
+							action: () => {
+								if (size < 2) return;
+								setSize((s) => s - 1);
+							},
+						},
+					],
+					body: [
+						{
+							label: 'Restart',
+							action: () => {
+								window.location.reload();
+							}
+						}
+					]
+				});
+			}}></div>
 			<br />
 
-			<input type="range" min="0" max="200" defaultValue={10} onChange={(e) => setSize(e.target.value)} />
 			{/* <textarea className={'lc'} ref={lcRef} value={lines}></textarea>
 
 			<textarea onScroll={(event) => {
