b13eb2513504c7fa9eb5c273dad2ee262b4bc966
XFaon <xfaonae@protonmail.com>
2022-03-20 15:17:40 -0400

    Delete packages directory

--- packages/desktop-uix/src/hooks/hooks.ts
+++
@@ -1,9 +0,0 @@
-import useThemeType from './useThemeType/useThemeType';
-import useAppWindow from './useAppWindow/useAppWindow';
-import useChannel from './useChannel/useChannel';
-import useRouter, {Route as UseRouterRoute} from './useRouter/useRouter';
-import useAppURL, {Settings as UseAppURLSettings} from './useAppURL/useAppURL';
-import useMenu, {Settings as UseMenuSettings} from './useMenu/useMenu';
-
-export {useThemeType, useAppWindow, useMenu, useAppURL, useRouter, useChannel};
-export {UseMenuSettings, UseAppURLSettings, UseRouterRoute};
--- packages/desktop/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {initialize} from '@electron/remote/main';
-export * as app from './app/app';
-export * as windowManager from './windowManager/windowManager';
-import Window, {Settings as WindowSettings, Errors as WindowErrors} from './window/Window';
-import Channel, {Errors as ChannelErrors} from './window/Channel';
-
-initialize();
-
-export {Window, WindowSettings, WindowErrors, Channel, ChannelErrors};
